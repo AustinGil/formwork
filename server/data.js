5d181d8e7d95a6a17b630cea6f3f835c7543f548
Austin Gil <hey@austingil.com>
2022-12-19 03:51:38 -0800

    Adding data

---
+++ server/data.js
@@ -0,0 +1,75 @@
+export const languages = [
+  {
+    id: 'html',
+    logo: '/img/html.svg',
+    name: 'HTML',
+    description: 'The bones of any good website',
+  },
+  {
+    id: 'css',
+    logo: '/img/css.svg',
+    name: 'CSS',
+    description: 'Styles to make your mamma proud',
+  },
+  {
+    id: 'js',
+    logo: '/img/javascript.svg',
+    name: 'JavaScript',
+    description: 'Fancy-pantsy movements and stuff',
+  },
+];
+
+export const pokemons = [
+  {
+    id: 'bulbasaur',
+    name: 'Bulbasaur',
+    img: 'img/bulbasaur.png',
+    description:
+      "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
+    stats: [
+      'Height: 2\' 04"',
+      'Weight: 15.2 lbs',
+      'Type: Grass/poison',
+      'Weaknesses: Fire, Psychic, Flying, Ice',
+    ],
+  },
+  {
+    id: 'charmander',
+    name: 'Charmander',
+    img: 'img/charmander.png',
+    description:
+      'The flame that burns at the tip of its tail is an indication of its emotions.The flame wavers when Charmander is enjoying itself.If the Pokémon becomes enraged, the flame burns fiercely.',
+    stats: [
+      'Height:1\' 08"',
+      'Weight: 19.8 lbs',
+      'Type: Water',
+      'Weaknesses: Grass, Electric',
+    ],
+  },
+  {
+    id: 'squirtle',
+    name: 'Squirtle',
+    img: 'img/squirtle.png',
+    description:
+      "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds.",
+    stats: [
+      'Height: 2\' 00"',
+      'Weight: 18.7 lbs',
+      'Type: Fire',
+      'Weaknesses: Water, Ground, Rock',
+    ],
+  },
+];
+
+export const richPeople = [
+  'Elon Musk',
+  'Jeff Bezos',
+  'Bernard Arnault',
+  'Gautam Adani',
+  'Bill Gates',
+  'Warren Buffett',
+  'Larry Page',
+  'Sergey Brin',
+  'Larry Ellison',
+  'Steve Ballmer',
+];
